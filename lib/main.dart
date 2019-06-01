3283446e747933074c205383b9312146913e7aca
Lucas <lucasdiogo1234@gmail.com>
2019-06-01 14:51:31 -0300

    Main alterado.

--- lib/main.dart
+++ lib/main.dart
@@ -8,7 +8,6 @@ import 'package:flutter_up/root_page.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -17,11 +16,12 @@ class MyApp extends StatelessWidget {
           primarySwatch: Colors.blue,
         ),
         debugShowCheckedModeBanner: false,
-        home: Login(),
+        home: RootPage(),
         routes: <String, WidgetBuilder>{
           '/login/login': (BuildContext context) =>  Login(),
           '/register/register': (BuildContext context) =>  Register(),
           '/register/password_recovery': (BuildContext context) =>  PasswordRecovery(),
+          '/home/': (BuildContext context) =>  MyHomePage(),
         });
   }
 }
