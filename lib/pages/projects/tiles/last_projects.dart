1aca7d7d6978bf242ddc0ebebff0f1db5311ee68
joaoh <joao_camargo10@hotmail.com>
2019-06-01 14:53:01 -0300

    Modificações nos projetos

--- lib/pages/projects/tiles/last_projects.dart
+++ lib/pages/projects/tiles/last_projects.dart
@@ -17,86 +17,75 @@ class _LastProjectsTileState extends State<LastProjectsTile> {
           width: double.infinity,
           child: Card(
               color: Colors.white,
-              child: Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Row(
-                  children: <Widget>[
-                    Padding(
-                      padding: EdgeInsets.only(right: 10.0),
-                      child: Container(
-                        width: 70.0,
-                        height: 70.0,
-                        decoration: new BoxDecoration(
-                            shape: BoxShape.circle,
-                        ),
-                        child: Stack(
-                          children: <Widget>[
-                            Positioned(
-                              top: 25.0,
-                              left: 12.0,
-                              child: Row(
-                                children: <Widget>[
-                                  Icon(Icons.star, size: 15.0,),
-                                  SizedBox(width: 5.0,),
-                                  Text("4.5")
-                                ],
-                              ),
-                            )
-                          ],
-                        ),
-                      ),
-                    ),
-                    Padding(
-                      padding: EdgeInsets.only(right: 10.0),
-                      child: Container(
-                        color: Colors.grey[300],
-                        height: 60.0,
-                        width: 0.5,
-                      ),
-                    ),
-                    Expanded(
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.start,
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: <Widget>[
-                          Text("Flutter Up"),
-                          SizedBox(
-                            height: 8.0,
+              child: InkWell(
+                onTap: (){
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => ProjectDescription(false)),
+                  );
+                },
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Row(
+                    children: <Widget>[
+                      Padding(
+                        padding: EdgeInsets.only(right: 10.0),
+                        child: Container(
+                          width: 70.0,
+                          height: 70.0,
+                          decoration: new BoxDecoration(
+                              shape: BoxShape.circle,
                           ),
-                          Text("UX/UI"),
-                          SizedBox(
-                            height: 8.0,
-                          ),
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          child: Stack(
                             children: <Widget>[
-                              Row(
-                                children: <Widget>[
-                                  Text("1.503", style: TextStyle(fontWeight: FontWeight.w700),),
-                                  Text(" vote(s)")
-                                ],
-                              ),
-                              Padding(
-                                padding: EdgeInsets.only(right: 10.0),
-                                child: GestureDetector(
-                                    onTap: () {
-                                      Navigator.push(
-                                        context,
-                                        MaterialPageRoute(
-                                            builder: (context) => ProjectDescription(true)),
-                                      );
-                                    },
-                                    child: Text(
-                                      "View",
-                                      style: TextStyle(color: Colors.blue),
-                                    )),
+                              Positioned(
+                                top: 25.0,
+                                left: 12.0,
+                                child: Row(
+                                  children: <Widget>[
+                                    Icon(Icons.star, size: 15.0,),
+                                    SizedBox(width: 5.0,),
+                                    Text("4.5")
+                                  ],
+                                ),
                               )
                             ],
                           ),
-                        ],
+                        ),
                       ),
-                    )
-                  ],
+                      Padding(
+                        padding: EdgeInsets.only(right: 10.0),
+                        child: Container(
+                          color: Colors.grey[300],
+                          height: 60.0,
+                          width: 0.5,
+                        ),
+                      ),
+                      Expanded(
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: <Widget>[
+                            Text("Flutter Up"),
+                            SizedBox(
+                              height: 8.0,
+                            ),
+                            Text("UX/UI"),
+                            SizedBox(
+                              height: 8.0,
+                            ),
+                            Row(
+                              children: <Widget>[
+                                Text("1.503", style: TextStyle(fontWeight: FontWeight.w700),),
+                                Text(" vote(s)")
+                              ],
+                            ),
+                          ],
+                        ),
+                      )
+                    ],
+                  ),
                 ),
               )),
           decoration: BoxDecoration(
--- lib/pages/projects/tiles/recent_projects.dart
+++ lib/pages/projects/tiles/recent_projects.dart
@@ -1,4 +1,6 @@
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_up/pages/project/details.dart';
 
 import '../project_description.dart';
 
@@ -17,86 +19,75 @@ class _RecentProjectsTileState extends State<RecentProjectsTile> {
           width: double.infinity,
           child: Card(
               color: Colors.white,
-              child: Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Row(
-                  children: <Widget>[
-                    Padding(
-                      padding: EdgeInsets.only(right: 10.0),
-                      child: Container(
-                        width: 70.0,
-                        height: 70.0,
-                        decoration: new BoxDecoration(
-                            shape: BoxShape.circle,
-                            border: Border.all(width: 0.3, color: Colors.grey[400])),
-                        child: Stack(
-                          children: <Widget>[
-                            Positioned(
-                              top: 25.0,
-                              left: 22.0,
-                              child: Text(
-                                "15h",
-                                style: TextStyle(
-                                    color: Colors.grey[700],
-                                    fontWeight: FontWeight.w700),
-                              ),
-                            )
-                          ],
-                        ),
-                      ),
-                    ),
-                    Padding(
-                      padding: EdgeInsets.only(right: 10.0),
-                      child: Container(
-                        color: Colors.grey[300],
-                        height: 60.0,
-                        width: 0.5,
-                      ),
-                    ),
-                    Expanded(
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.start,
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: <Widget>[
-                          Text("Flutter Up"),
-                          SizedBox(
-                            height: 8.0,
-                          ),
-                          Text("UX/UI"),
-                          SizedBox(
-                            height: 8.0,
-                          ),
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              child: InkWell(
+                onTap: (){
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => ProjectDescription(true)),
+                  );
+                },
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Row(
+                    children: <Widget>[
+                      Padding(
+                        padding: EdgeInsets.only(right: 10.0),
+                        child: Container(
+                          width: 70.0,
+                          height: 70.0,
+                          decoration: new BoxDecoration(
+                              shape: BoxShape.circle,
+                              border: Border.all(width: 0.3, color: Colors.grey[400])),
+                          child: Stack(
                             children: <Widget>[
-                              Row(
-                                children: <Widget>[
-                                  Icon(Icons.star, size: 15.0,),
-                                  SizedBox(width: 5.0,),
-                                  Text("4.5")
-                                ],
-                              ),
-                              Padding(
-                                padding: EdgeInsets.only(right: 10.0),
-                                child: GestureDetector(
-                                    onTap: () {
-                                      Navigator.push(
-                                        context,
-                                        MaterialPageRoute(
-                                            builder: (context) => ProjectDescription(true)),
-                                      );
-                                    },
-                                    child: Text(
-                                      "View",
-                                      style: TextStyle(color: Colors.blue),
-                                    )),
+                              Positioned(
+                                top: 25.0,
+                                left: 22.0,
+                                child: Text(
+                                  "15h",
+                                  style: TextStyle(
+                                      color: Colors.grey[700],
+                                      fontWeight: FontWeight.w700),
+                                ),
                               )
                             ],
                           ),
-                        ],
+                        ),
                       ),
-                    )
-                  ],
+                      Padding(
+                        padding: EdgeInsets.only(right: 10.0),
+                        child: Container(
+                          color: Colors.grey[300],
+                          height: 60.0,
+                          width: 0.5,
+                        ),
+                      ),
+                      Expanded(
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: <Widget>[
+                            Text("Flutter Up"),
+                            SizedBox(
+                              height: 8.0,
+                            ),
+                            Text("UX/UI"),
+                            SizedBox(
+                              height: 8.0,
+                            ),
+                            Row(
+                              children: <Widget>[
+                                Icon(Icons.star, size: 15.0,),
+                                SizedBox(width: 5.0,),
+                                Text("4.5")
+                              ],
+                            ),
+                          ],
+                        ),
+                      )
+                    ],
+                  ),
                 ),
               )),
           decoration: BoxDecoration(
