239085bdd63ccc928454321d8e8a19e8f0b527d6
Rodrigolmg <rodrigo.leitemg@gmail.com>
2019-06-01 13:52:32 -0300

    Tela home e category_list adicionada

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,86 +1,125 @@
 import 'package:flutter/material.dart';
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
+class Home extends StatefulWidget {
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  _HomeState createState() => _HomeState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _HomeState extends State<Home> {
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          centerTitle: true,
+          title: Text("Flutter UP!"),
+          actions: <Widget>[
+            IconButton(
+              onPressed: (){},
+              icon: Icon(Icons.search),
+            )
+          ],
+        ),
+        body: Padding(
+          padding: EdgeInsets.only(top: 80.0),
+          child: Column(
+            children: <Widget>[
+              Padding(
+                padding: EdgeInsets.only(bottom: 100.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: <Widget>[
+                    Stack(
+                      children: <Widget>[
+                        Positioned(
+                          top: 40,
+                          left: 50,
+                          child: Container(
+                            width: 30.0,
+                            height: 30.0,
+                            decoration: new BoxDecoration(
+                              shape: BoxShape.circle,
+                            ),
+                            child: CircularProgressIndicator(),
+                          ),
+                        ),
+                        Container(
+                            width: 100.0,
+                            height: 100.0,
+                            decoration: new BoxDecoration(
+                                shape: BoxShape.circle,
+                                image: new DecorationImage(
+                                    fit: BoxFit.cover,
+                                    image: AssetImage("assets/flutter-icon-jpeg.jpg")
+                                  //AssetImage("assets/flutter-icon-jpeg.jpg")
+                                )
+                            )
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+              Container(
+                decoration: BoxDecoration(color: Colors.blue),
+                child: Text("Mensagem", style: TextStyle(color: Colors.white),),
+              ),
+              Padding(
+                padding: EdgeInsets.only(right: 10.0),
+                child: Row(
+                  children: <Widget>[
+                    _buildCategoryItens("1"),
+                    _buildCategoryItens("1"),
+                  ],
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.only(right: 10.0),
+                child: Padding(
+                  padding: EdgeInsets.only(top: 50.0),
+                  child: Row(
+                    children: <Widget>[
+                      _buildCategoryItens("1"),
+                      _buildCategoryItens("1"),
+                    ],
+                  ),
+                )
+              ),
+            ],
+          ),
+        )
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
+    );
+  }
+}
+
+Widget _buildCategoryItens(String level){
+  return Padding(
+      padding: EdgeInsets.only(left: 87.0),
+      child: Container(
+        width: 70.0,
+        height: 70.0,
+        decoration: new BoxDecoration(
+            shape: BoxShape.circle,
+            border: Border.all(width: 0.3, color: Colors.grey[400])),
+        child: Stack(
           children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
+            Positioned(
+              top: 25.0,
+              left: 30.0,
+              child: Text(
+                level,
+                style: TextStyle(
+                    color: Colors.grey[700],
+                    fontWeight: FontWeight.w700),
+              ),
+            )
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
-  }
+  );
 }
